7607dee00f2874c3d7be342e8cb11a85e82939cb
Prokop <prokop.schield@protonmail.com>
2025-10-15 19:57:32 +0200

    feat: HtreeKey::try_to_bytes

--- src/key/mod.rs
+++ src/key/mod.rs
@@ -1,16 +1,28 @@
 mod error;
 mod variants;
 
+use bytes::Bytes;
 pub use error::HtreeKeyError;
 use ps_hkey::{Hkey, Store};
 use ps_uuid::UUID;
 pub use variants::*;
 
 pub trait HtreeKey {
+    /// Serializes this [`HtreeKey`] and returns its byte representation.
+    /// # Errors
+    /// Returns an [`HtreeKeyError`] if serialization fails.
+    fn try_to_bytes<S: Store>(&self, store: &S) -> Result<Bytes, HtreeKeyError<S>> {
+        Ok(self.try_to_uuid(store)?.as_bytes().to_vec().into())
+    }
+
     /// Stores this [`HtreeKey`] and returns its [`Hkey`].
     /// # Errors
     /// Returns an [`HtreeKeyError`] if serialization or storage fails.
-    fn try_to_hkey<S: Store>(&self, store: &S) -> Result<Hkey, HtreeKeyError<S>>;
+    fn try_to_hkey<S: Store>(&self, store: &S) -> Result<Hkey, HtreeKeyError<S>> {
+        store
+            .put(&self.try_to_bytes(store)?)
+            .map_err(HtreeKeyError::Store)
+    }
 
     /// Interprets this [`HtreeKey`] as a [`UUID`].
     /// # Errors
