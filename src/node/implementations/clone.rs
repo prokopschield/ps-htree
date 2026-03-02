d4b608451836f02e46553714dcefcb336ec15b59
Prokop Schield <prokop.schield@protonmail.com>
2026-03-02 14:49:15 +0100

    chore: remove obsolete comment

--- src/node/implementations/clone.rs
+++ src/node/implementations/clone.rs
@@ -6,9 +6,6 @@ impl<T> Clone for HtreeNode<T> {
     /// This returns a new handle referencing the same
     /// underlying distributed node state, not a deep copy
     /// of its contents.
-    ///
-    /// To duplicate the tree’s contents, use
-    /// [`HtreeNode::deep_clone`].
     fn clone(&self) -> Self {
         Self {
             inner: self.inner.clone(),
